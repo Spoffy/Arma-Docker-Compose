4e2533c2ba4cd05a7a1225970b8700ad8a8853a2
Spoffy <contact@spoffy.net>
2019-12-27 16:49:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Docker-compose files designed to work with arma3-docker.
+
+# Usage
+
+Use:
+`git submodule init && git submodule update`
+
+To install arma3-docker
---
+++ makefolders.sh
@@ -0,0 +1,11 @@
+BASE=/opt
+ARMAROOT=$BASE/arma3
+
+mkdir $ARMAROOT
+
+mkdir $ARMAROOT/keys
+mkdir $ARMAROOT/mods
+mkdir $ARMAROOT/mpmissions
+mkdir $ARMAROOT/server
+mkdir $ARMAROOT/params
+mkdir $ARMAROOT/profiles
